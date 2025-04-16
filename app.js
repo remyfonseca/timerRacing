0d5e6c3495abdd57c063dde84e08fe3fc67b8924
Remy Neto <remyydev@gmail.com>
2025-04-16 20:55:00 -0300

    feat: recursos para adição de voltas e os tempos de cada volta

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const timer = document.querySelector("#timer");
 const startButton = document.querySelector("#start");
 const stopButton = document.querySelector("#stop");
+const resetFullButton = document.querySelector("#resetFull");
+const lapButton = document.querySelector("#lap");
+const listLaps = document.querySelector("#laps");
 const resetButton = document.querySelector("#reset");
 let horas = 0;
 let minutos = 0;
 let segundos = 0;
 let milisegundos = 0;
 let intervalo = null;
+let volta = 1;
 
 const formataTempo = (valor) =>{
     return valor < 10 ? "0" + valor : valor;
@@ -28,6 +32,7 @@ const atualizaTempo = () => {
     }
     timer.textContent = `${formataTempo(horas)}:${formataTempo(minutos)}:${formataTempo(segundos)}:${formataTempo(milisegundos / 10)}`;
 }
+
 const aplicarEstilos = (corTexto, boxShadow, transicao, botaoStartText, botaoStopText) => {
     timer.style.color = corTexto;
     timer.style.boxShadow = boxShadow;
@@ -35,6 +40,7 @@ const aplicarEstilos = (corTexto, boxShadow, transicao, botaoStartText, botaoSto
     startButton.textContent = botaoStartText;
     stopButton.textContent = botaoStopText;
 };
+
 startButton.addEventListener("click", () => {
     if (!intervalo) {
         intervalo = setInterval(atualizaTempo, 10); 
@@ -49,7 +55,7 @@ stopButton.addEventListener("click", () => {
     aplicarEstilos("var(--cor-erro)", "0 0 10px var(--cor-erro)", "box-shadow 0.5s ease-in-out", "Iniciar", "Parado");
 });
 
-resetButton.addEventListener("click", () => {  
+resetButton.addEventListener("click", () => {
     clearInterval(intervalo);
     intervalo = null;
     horas = 0;
@@ -57,5 +63,30 @@ resetButton.addEventListener("click", () => {
     segundos = 0;
     milisegundos = 0;
     timer.textContent =  "00:00:00:00";
-    aplicarEstilos("", "0 0 10px var(--cor-timer-texto)", "box-shadow 0.5s ease-in-out", "Iniciar", "Parar");
+    aplicarEstilos("", "0 0 10px var(--cor-btn-reset)", "box-shadow 0.5s ease-in-out", "Iniciar", "Parar");
+});
+
+resetFullButton.addEventListener("click", () => {  
+    clearInterval(intervalo);
+    intervalo = null;
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+    milisegundos = 0;
+    timer.textContent =  "00:00:00:00";
+    listLaps.innerHTML = "";
+    volta = 1;
+    aplicarEstilos("", "0 0 10px var(--cor-btn-resetFull)", "box-shadow 0.5s ease-in-out", "Iniciar", "Parar");
+});
+
+lapButton.addEventListener("click", () => {
+    if (timer.textContent === "00:00:00:00") {
+        alert("Você não pode adicionar voltas com o cronômetro zerado!");
+        return; 
+    }
+    aplicarEstilos("", "0 0 10px var(--cor-btn-lap)", "box-shadow 0.5s ease-in-out", "Contando", "Parar");
+    const laps = document.createElement("li");
+    laps.textContent = `Tempo ${volta}: ${formataTempo(horas)}:${formataTempo(minutos)}:${formataTempo(segundos)}:${formataTempo(milisegundos / 10)}`;
+    listLaps.appendChild(laps);
+    volta++;
 });
