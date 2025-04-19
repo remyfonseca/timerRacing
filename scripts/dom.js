8584b9a826842dab2cb86b3f07e11cbd0a7f942e
Remy Neto <remyydev@gmail.com>
2025-04-18 21:58:29 -0300

    feat: adicionar módulo dom.js para seleção de elelementos

---
+++ scripts/dom.js
@@ -0,0 +1,7 @@
+export const timer = document.querySelector("#timer");
+export const startButton = document.querySelector("#start");
+export const stopButton = document.querySelector("#stop");
+export const resetFullButton = document.querySelector("#resetFull");
+export const lapButton = document.querySelector("#lap");
+export const listLaps = document.querySelector("#laps");
+export const resetButton = document.querySelector("#reset");
